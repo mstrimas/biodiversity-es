8c4a2ca348e9e7a6575a0220d70fc598bb5f0b4f
ricschuster <mail@richard-schuster.com>
2020-09-30 07:25:47 -0700

    caltulate_targets function based on Hanson et al. 2020

--- 04_prioritize.R
+++ 04_prioritize.R
@@ -5,6 +5,32 @@ library(prioritizr)
 library(gurobi)
 library(tidyverse)
 
+# define target function
+calculate_targets <- function(x, minimum_range_size = 1000,
+                              proportion_at_minimum_range_size = 1,
+                              maximum_range_size = 250000,
+                              proportion_at_maximum_range_size = 0.1,
+                              cap_range_size = 10000000,
+                              cap_target_amount = 1000000) {
+  # initialization
+  out <- rep(NA_real_, length(x))
+  pos <- which(is.finite(x))
+  # set targets below threshold
+  out[pos] <- stats::approx(
+    x = log10(c(minimum_range_size, maximum_range_size)),
+    y = c(proportion_at_minimum_range_size,
+          proportion_at_maximum_range_size),
+    xout = log10(x[pos]), method = "linear", rule = 2)$y
+  # set caps
+  cap_pos <- which(x > cap_range_size)
+  if (length(cap_pos) > 0) {
+    out[cap_pos] <- cap_target_amount / x[cap_pos]
+  }
+  # return values
+  return(out)
+}
+
+
 # parameters
 n_cores <- 10
 resolution <- 10
@@ -48,8 +74,7 @@ max_total <- resolution^2
 features <- features %>% 
   mutate(aoh = ifelse(type != "es", total / max_total, NA_real_)) %>% 
   # set target based on aoh
-  mutate(prop = 1.1 - 1e-4 * aoh,
-         prop = pmin(pmax(prop_msm, 0.1), 1.0))
+  mutate(prop = calculate_targets(aoh))
 
 
 # prioritize ---- 
